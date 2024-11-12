87c9f89a4ce7d5a5ce930f4e9d3dd2c66481bf36
valziyan <valziyan@gmail.com>
2024-11-12 19:18:21 +0800

    first commit or depende sa imuha trip

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+<nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <h1>Contact Page</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "im101";
+
+$name = $_POST['name'];
+
+try {
+    // create $conn and create a new connection similar mysql -u root -p
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // set your SQL Command
+    if(isset($name)) {
+        $variableNameSQL = "INSERT INTO user (Name) values ('$name')";
+
+        // execute the command
+        echo $conn->exec($variableNameSQL);
+        echo "Record added successfully";
+    }
+
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <h1>Home Page</h1>
+        
+    <form action="" method="post">
+        Name: <input type="text" name="name"><br>
+        <input type="submit" value="SAVE">
+    </form>
+
+    <script>
+        console.log("my first html form");
+    </script>
+
+</body>
+</html>
